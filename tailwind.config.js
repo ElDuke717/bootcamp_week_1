974faee5df8d953443d36dd8ceb771fdb41eb5e5
Nick Huemmer <njhuemmer@gmail.com>
2022-04-08 13:25:48 -0400

    added text and tailwind markup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Inter var', 'sans-serif'],
+      serif: ['Inter var', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
